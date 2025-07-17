c37337a4628f3640a0d0ae3ec42b1a72efbe93a5
Abhishek Anand <abhishekanand3672@gmail.com>
2025-07-17 13:10:49 +0530

    creating databse

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();                                  {/*have to initialize dotenv before using it*/}
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB!")
+}).catch((err) => {
+    console.log("Error")
+});
 
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running of port 3000');
+
+app.listen(3000, () => {                          {/* calling listen function/method */}
+    console.log('Server is running on port 3000');
 })
