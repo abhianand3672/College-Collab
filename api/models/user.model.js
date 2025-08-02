72a5c27eb613e83609c7b10ca3fda0e59c7ae05c
Abhishek Anand <abhishekanand3672@gmail.com>
2025-08-02 15:27:46 +0530

    Changing Models

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -41,10 +41,6 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    // avatar:{
-    //   type: String,
-    //   default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    // },
   },
   { timestamps: true }   //time of creation and updation
 );
